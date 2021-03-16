a7e6c6ed9febe5819cb18f7bbdcf87a6ad8a541b
Connor Walden <connormwalden@gmail.com>
2021-03-16 20:57:48 +1030

    Finished back-end, tested all api routes with insomnia

--- models/index.js
+++ models/index.js
@@ -5,27 +5,16 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-Product.belongsTo(Category, {
-  foreignKey: 'id'
-});
+Product.belongsTo(Category);
 
 // Categories have many Products
-Category.hasMany(Product, {
-  foreignKey: 'category_id',
-  onDelete: 'CASCADE',
-});
+Category.hasMany(Product);
 
 // Products belongToMany Tags (through ProductTag)
-ProductTag.belongsToMany(Tag, {
-  through: "id",
-  onDelete: 'CASCADE',
-});
+Product.belongsToMany(Tag, { through: ProductTag });
 
 // Tags belongToMany Products (through ProductTag)
-Tag.belongsToMany(ProductTag, {
-  through: 'product_id',
-  onDelete: 'CASCADE',
-});
+Tag.belongsToMany(Product, { through: ProductTag });
 
 module.exports = {
   Product,
--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -3,12 +3,14 @@ const { Tag, Product, ProductTag } = require('../../models');
 
 // The `/api/tags` endpoint
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all tags
   // be sure to include its associated Product data
   try {
     const tags = await Tag.findAll({
-      include: [{ model: Product }],
+      include: [
+        { model: Product } 
+      ],
     });
     res.status(200).json(tags);  
   } catch(err) {
@@ -16,7 +18,7 @@ router.get('/', (req, res) => {
   }
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product datatry {
   try {
@@ -37,7 +39,7 @@ router.get('/:id', (req, res) => {
   }
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // create a new tag
   try {
     const tagData = await Tag.create(req.body);
@@ -47,7 +49,7 @@ router.post('/', (req, res) => {
   }
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a tag's name by its `id` value
   try {
     const tagData = await Tag.update(req.body, { where: { id: req.params.id } });
@@ -57,7 +59,7 @@ router.put('/:id', (req, res) => {
   }
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete on tag by its `id` value
   try {
     const tagData = await Tag.destroy({
